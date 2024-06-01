82bf120e621dc36620a8bdb1266b92ad133f480a
Xinggg02 <169126551+Xinggg02@users.noreply.github.com>
2024-06-01 23:30:46 +0800

    Update 3.py

--- 3.py
+++ 3.py
@@ -17,7 +17,7 @@ html_temp = """
 stc.html(html_temp)
 
 ## 读取Excel文件
-df_original = pd.read_excel('kbars_2330_2022-01-01-2022-11-18.xlsx')
+df_original = pd.read_excel('(1215)2023_2024.xlsx')
 df_original = df_original.drop('Unnamed: 0', axis=1)
 
 ##### 選擇資料區間
